a9282d1b46f775fb80d25e49beb53a209ebded55
Angkur <angkurdhar1@gmail.com>
2025-11-23 05:37:15 +0600

    47th

--- Recursion/Nqueen.java
+++ Recursion/Nqueen.java
@@ -1,17 +1,47 @@
 public class Nqueen {
+    static int count = 0;
+    public static boolean isSafe(char board[][], int row, int col) {
+        int i,j;
+        //up search
+        for(i = row-1; i >= 0; i--){
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        //up left diagonal
+        for(i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        //up right diagonal
+        for(i = row-1, j = col+1; i >= 0 && j < board.length; i--, j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void nQueens(char board[][], int row) {
         //base case
         if(row == board.length) {
             printBoard(board);
+            count++;
             return;
         }
 
         //main body
         for(int j = 0; j < board.length; j++) {
-            board[row][j] = 'Q';
-            nQueens(board, row+1);
-            board[row][j] = '.';
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                nQueens(board, row+1);
+                board[row][j] = '.';
+            }
+            
         }
     }
 
@@ -25,7 +55,7 @@ public class Nqueen {
         }
     }
     public static void main(String[] args) {
-        int n = 2;
+        int n = 5;
         char board[][] = new char[n][n];
 
         for(int i = 0; i<n; i++) {
@@ -34,5 +64,6 @@ public class Nqueen {
             }
         }
         nQueens(board, 0);
+        System.out.println("Total possibility: "+count);
     }
 }
