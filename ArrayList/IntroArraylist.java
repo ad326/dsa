30cf975dcbc7e6f104dbead9f24fbc9ed2b2d738
Angkur <angkurdhar1@gmail.com>
2025-11-29 06:54:59 +0600

    48th

---
+++ ArrayList/IntroArraylist.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class IntroArraylist {
+    public static void main(String[] args) {
+
+        //Initialization of arraylist
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //Add elements
+        list.add(1); //O(1)
+        list.add(1,2); //O(n)
+
+        for(int i = 0 ; i<5; i++) {
+            list.add(3+i);
+        }
+
+        System.out.println(list);
+
+        //Get elements
+        System.out.println(list.get(4)); //O(1)
+
+        //Remove elements
+        list.remove(3);   //O(n)
+        System.out.println(list);
+
+        //Set elements
+        list.set(3, 4); //O(n)
+        System.out.println(list);
+
+        //Search elements
+        System.out.println(list.contains(4)); //O(n)
+
+        //Size of arraylist
+        System.out.println("The size of this arrayList is: "+list.size());
+
+    }
+}
